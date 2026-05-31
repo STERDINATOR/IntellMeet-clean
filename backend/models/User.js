9fc75932fef76e2ad1c1b6a6b28ae6e661e9bf90
Saiesh <95605065+STERDINATOR@users.noreply.github.com>
2026-05-31 17:47:45 +0530

    Delete backend directory

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-// models/User.js - Example user model
-// Implement your Mongoose schemas here
-
-// Example structure:
-// const userSchema = new Schema({
-//   username: String,
-//   email: String,
-//   password: String,
-// });
